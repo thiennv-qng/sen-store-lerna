7b9826733c10d548fc16443037811db8ba95c4dd
thiennv <thiennguyen@descartes.network>
2022-07-19 10:04:01 +0700

    update route

--- packages/sen-store/src/view/index.tsx
+++ packages/sen-store/src/view/index.tsx
@@ -1,41 +1,33 @@
-import { Col, Row } from 'antd'
-import BannerHeader from './banner/header'
-import BrowseApps from './browseApps'
-import HotDApps from './hotDApps'
+import { Layout } from 'antd'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Market from './market'
-import MostPopular from './mostPopular'
-import NewBuilderApps from './newBuilderApps'
 import PopularTwitter from './popularTwitter'
 import Trending from './trending'
 
+import configs from 'configs'
+
+const {
+  manifest: { appId },
+} = configs
+
 const View = () => {
   return (
-    <Row gutter={[24, 24]}>
-      <Col span={24}>
-        <Market />
-      </Col>
-      <Col span={24}>
-        <BannerHeader />
-      </Col>
-      <Col span={24}>
-        <HotDApps />
-      </Col>
-      <Col span={24}>
-        <MostPopular />
-      </Col>
-      <Col span={24}>
-        <PopularTwitter />
-      </Col>
-      <Col span={24}>
-        <Trending />
-      </Col>
-      <Col span={24}>
-        <BrowseApps />
-      </Col>
-      <Col span={24}>
-        <NewBuilderApps />
-      </Col>
-    </Row>
+    <Layout>
+      <Layout.Content>
+        <Switch>
+          <Route exact path={`/app/${appId}/`} component={Market} />
+          <Route exact path={`/app/${appId}/details`} component={Trending} />
+          <Route
+            exact
+            path={`/app/${appId}/details/:id`}
+            component={PopularTwitter}
+          />
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Layout.Content>
+    </Layout>
   )
 }
 
